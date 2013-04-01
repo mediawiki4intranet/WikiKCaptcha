5638cedce3d33f1e90e19d9a0e58727fbb37cc8c
Vitaliy Filippov <vitalif@yourcmc.ru>
2013-04-01 14:57:27 +0400

    Update KCAPTCHA module to 2.0

--- util/kcaptcha.php
+++ util/kcaptcha.php
@@ -1,25 +1,26 @@
 <?php
 
-# KCAPTCHA PROJECT VERSION 1.2.3
+# KCAPTCHA PROJECT VERSION 2.0
 
 # Automatic test to tell computers and humans apart
 
-# Copyright by Kruglov Sergei, 2006
+# Copyright by Kruglov Sergei, 2006, 2007, 2008, 2011
 # www.captcha.ru, www.kruglov.ru
 
-# System requirements: PHP w/ GD
+# System requirements: PHP 4.0.6+ w/ GD
 
-# KCAPTCHA is a free software. You can freely use it for building own site or software.
+# KCAPTCHA is a free software. You can freely use it for developing own site or software.
 # If you use this software as a part of own sofware, you must leave copyright notices intact or add KCAPTCHA copyright notices to own.
 # As a default configuration, KCAPTCHA has a small credits text at bottom of CAPTCHA image.
 # You can remove it, but I would be pleased if you left it. ;)
 
-# See kcaptcha_config.php for customizing
+# See kcaptcha_config.php for customization
 
 class KCAPTCHA{
 
 	// generates keystring and image
 	function KCAPTCHA(){
+
 		require_once(dirname(__FILE__).'/kcaptcha_config.php');
 		$fonts=array();
 		$fontsdir_absolute=dirname(__FILE__).'/'.$fontsdir;
@@ -30,25 +31,27 @@ class KCAPTCHA{
 				}
 			}
 		    closedir($handle);
-		}	
-	
+		}
+
 		$alphabet_length=strlen($alphabet);
-		
-		while(true){
+
+		do{
 			// generating random keystring
 			while(true){
 				$this->keystring='';
 				for($i=0;$i<$length;$i++){
 					$this->keystring.=$allowed_symbols{mt_rand(0,strlen($allowed_symbols)-1)};
 				}
-				if(!preg_match('/cp|cb|ck|c6|c9|rn|rm|co|do/',$this->keystring)) break;
+				if(!preg_match('/cp|cb|ck|c6|c9|rn|rm|mm|co|do|cl|db|qp|qb|dp|ww/', $this->keystring)) break;
 			}
-		
-			$font_file=$fonts[mt_rand(0,count($fonts)-1)];
+
+			$font_file=$fonts[mt_rand(0, count($fonts)-1)];
 			$font=imagecreatefrompng($font_file);
-			$black=imagecolorallocate($font,0,0,0);
+			imagealphablending($font, true);
+
 			$fontfile_width=imagesx($font);
 			$fontfile_height=imagesy($font)-1;
+
 			$font_metrics=array();
 			$symbol=0;
 			$reading_symbol=false;
@@ -71,33 +74,39 @@ class KCAPTCHA{
 				}
 			}
 
-			$img=imagecreatetruecolor($width,$height);
-			$white=imagecolorallocate($img,255,255,255);
-			$black=imagecolorallocate($img,0,0,0);
+			$img=imagecreatetruecolor($width, $height);
+			imagealphablending($img, true);
+			$white=imagecolorallocate($img, 255, 255, 255);
+			$black=imagecolorallocate($img, 0, 0, 0);
 
-			imagefilledrectangle ($img,0,0,$width-1,$height-1,$white);
+			imagefilledrectangle($img, 0, 0, $width-1, $height-1, $white);
 
 			// draw text
 			$x=1;
+			$odd=mt_rand(0,1);
+			if($odd==0) $odd=-1;
 			for($i=0;$i<$length;$i++){
 				$m=$font_metrics[$this->keystring{$i}];
 
-				$y=mt_rand(-$fluctuation_amplitude, $fluctuation_amplitude)+($height-$fontfile_height)/2+2;
+				$y=(($i%2)*$fluctuation_amplitude - $fluctuation_amplitude/2)*$odd
+					+ mt_rand(-round($fluctuation_amplitude/3), round($fluctuation_amplitude/3))
+					+ ($height-$fontfile_height)/2;
 
 				if($no_spaces){
 					$shift=0;
 					if($i>0){
-						$shift=1000;
-						for($sy=1;$sy<$fontfile_height-15;$sy+=2){
-							for($sx=$m['start']-1;$sx<$m['end'];$sx++){
+						$shift=10000;
+						for($sy=3;$sy<$fontfile_height-10;$sy+=1){
+							for($sx=$m['start']-1;$sx<$m['end'];$sx+=1){
 				        		$rgb=imagecolorat($font, $sx, $sy);
 				        		$opacity=$rgb>>24;
 								if($opacity<127){
 									$left=$sx-$m['start']+$x;
 									$py=$sy+$y;
-									for($px=min($left,$width-1);$px>$left-15 && $px>=0;$px--){
+									if($py>$height) break;
+									for($px=min($left,$width-1);$px>$left-200 && $px>=0;$px-=1){
 						        		$color=imagecolorat($img, $px, $py) & 0xff;
-										if($color+$opacity<190){
+										if($color+$opacity<170){ // 170 - threshold
 											if($shift>$left-$px){
 												$shift=$left-$px;
 											}
@@ -108,25 +117,40 @@ class KCAPTCHA{
 								}
 							}
 						}
+						if($shift==10000){
+							$shift=mt_rand(4,6);
+						}
+
 					}
 				}else{
 					$shift=1;
 				}
-				imagecopy($img,$font,$x-$shift,$y,$m['start'],1,$m['end']-$m['start'],$fontfile_height);
+				imagecopy($img, $font, $x-$shift, $y, $m['start'], 1, $m['end']-$m['start'], $fontfile_height);
 				$x+=$m['end']-$m['start']-$shift;
 			}
-			if($x<$width-10) break; // fit in canvas
-			
+		}while($x>=$width-10); // while not fit in canvas
+
+		//noise
+		$white=imagecolorallocate($font, 255, 255, 255);
+		$black=imagecolorallocate($font, 0, 0, 0);
+		for($i=0;$i<(($height-30)*$x)*$white_noise_density;$i++){
+			imagesetpixel($img, mt_rand(0, $x-1), mt_rand(10, $height-15), $white);
 		}
+		for($i=0;$i<(($height-30)*$x)*$black_noise_density;$i++){
+			imagesetpixel($img, mt_rand(0, $x-1), mt_rand(10, $height-15), $black);
+		}
+
+
 		$center=$x/2;
 
 		// credits. To remove, see configuration file
-		$img2=imagecreatetruecolor($width, $height+($show_credits?'12':0));
+		$img2=imagecreatetruecolor($width, $height+($show_credits?12:0));
 		$foreground=imagecolorallocate($img2, $foreground_color[0], $foreground_color[1], $foreground_color[2]);
 		$background=imagecolorallocate($img2, $background_color[0], $background_color[1], $background_color[2]);
+		imagefilledrectangle($img2, 0, 0, $width-1, $height-1, $background);
 		imagefilledrectangle($img2, 0, $height, $width-1, $height+12, $foreground);
 		$credits=empty($credits)?$_SERVER['HTTP_HOST']:$credits;
-		imagestring($img2, 2, $width/2-ImageFontWidth(2)*strlen($credits)/2, $height-2, $credits, $background);
+		imagestring($img2, 2, $width/2-imagefontwidth(2)*strlen($credits)/2, $height-2, $credits, $background);
 
 		// periods
 		$rand1=mt_rand(750000,1200000)/10000000;
@@ -134,25 +158,23 @@ class KCAPTCHA{
 		$rand3=mt_rand(750000,1200000)/10000000;
 		$rand4=mt_rand(750000,1200000)/10000000;
 		// phases
-		$rand5=mt_rand(0,3141592)/500000;
-		$rand6=mt_rand(0,3141592)/500000;
-		$rand7=mt_rand(0,3141592)/500000;
-		$rand8=mt_rand(0,3141592)/500000;
+		$rand5=mt_rand(0,31415926)/10000000;
+		$rand6=mt_rand(0,31415926)/10000000;
+		$rand7=mt_rand(0,31415926)/10000000;
+		$rand8=mt_rand(0,31415926)/10000000;
 		// amplitudes
 		$rand9=mt_rand(330,420)/110;
-		$rand10=mt_rand(330,450)/110;
+		$rand10=mt_rand(330,450)/100;
 
 		//wave distortion
+
 		for($x=0;$x<$width;$x++){
 			for($y=0;$y<$height;$y++){
 				$sx=$x+(sin($x*$rand1+$rand5)+sin($y*$rand3+$rand6))*$rand9-$width/2+$center+1;
 				$sy=$y+(sin($x*$rand2+$rand7)+sin($y*$rand4+$rand8))*$rand10;
-		
+
 				if($sx<0 || $sy<0 || $sx>=$width-1 || $sy>=$height-1){
-					$color=255;
-					$color_x=255;
-					$color_y=255;
-					$color_xy=255;
+					continue;
 				}else{
 					$color=imagecolorat($img, $sx, $sy) & 0xFF;
 					$color_x=imagecolorat($img, $sx+1, $sy) & 0xFF;
@@ -160,19 +182,18 @@ class KCAPTCHA{
 					$color_xy=imagecolorat($img, $sx+1, $sy+1) & 0xFF;
 				}
 
-				if($color==0 && $color_x==0 && $color_y==0 && $color_xy==0){
+				if($color==255 && $color_x==255 && $color_y==255 && $color_xy==255){
+					continue;
+				}else if($color==0 && $color_x==0 && $color_y==0 && $color_xy==0){
 					$newred=$foreground_color[0];
 					$newgreen=$foreground_color[1];
 					$newblue=$foreground_color[2];
-				}else if($color==255 && $color_x==255 && $color_y==255 && $color_xy==255){
-					$newred=$background_color[0];
-					$newgreen=$background_color[1];
-					$newblue=$background_color[2];	
 				}else{
 					$frsx=$sx-floor($sx);
 					$frsy=$sy-floor($sy);
 					$frsx1=1-$frsx;
 					$frsy1=1-$frsy;
+
 					$newcolor=(
 						$color*$frsx1*$frsy1+
 						$color_x*$frsx*$frsy1+
@@ -188,10 +209,14 @@ class KCAPTCHA{
 					$newblue=$newcolor0*$foreground_color[2]+$newcolor*$background_color[2];
 				}
 
-				imagesetpixel($img2, $x, $y, imagecolorallocate($img2,$newred,$newgreen,$newblue));
+				imagesetpixel($img2, $x, $y, imagecolorallocate($img2, $newred, $newgreen, $newblue));
 			}
 		}
 
+		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+		header('Cache-Control: no-store, no-cache, must-revalidate');
+		header('Cache-Control: post-check=0, pre-check=0', FALSE);
+		header('Pragma: no-cache');
 		if(function_exists("imagejpeg")){
 			header("Content-Type: image/jpeg");
 			imagejpeg($img2, null, $jpeg_quality);
@@ -209,6 +234,3 @@ class KCAPTCHA{
 		return $this->keystring;
 	}
 }
-
-
-?>
